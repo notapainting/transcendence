29438a166ab681bbb72f370992256c8877eb1c38
titi-rex <13titirex@pm.me>
2024-02-20 15:22:10 +0100

    clean

--- docker/django_dev_db/django/src/app_name/urls.py
+++ docker/django_dev_db/django/src/app_name/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for app project.
+URL configuration for app_name project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
