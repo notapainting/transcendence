9598f99529720f4b9e139e04f8a74b17f2ef2031
Louisa4124 <louisaaa14@gmail.com>
2024-02-21 13:55:25 +0100

    FIX : reload of single page

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 
-var connexion = document.getElementById("anon");
-
-connexion.addEventListener('click', launchSite);
-
-function launchSite()
-{
-    hideConnexionContent();
-    showNavBar();
-    showAccueilContent();
-}
+console.log(location.hash)
 
 function hideConnexionContent() {
     document.getElementById('connexion-content').style.display = 'none';
@@ -16,26 +7,35 @@ function hideConnexionContent() {
 
 function hideJeuContent() {
     document.getElementById('jeu-content').style.display = 'none';
+    if ( document.getElementById("jeuID").classList.contains('active') )
+        document.getElementById("jeuID").classList.toggle('active');
 }
 
 function hideChatContent() {
     document.getElementById('chat-content').style.display = 'none';
+    if ( document.getElementById("chatID").classList.contains('active') )
+        document.getElementById("chatID").classList.toggle('active');
 }
 
 function hideAccueilContent() {
     document.getElementById('accueil-content').style.display = 'none';
+    if ( document.getElementById("accID").classList.contains('active') )
+        document.getElementById("accID").classList.toggle('active');
 }
 
 function showJeuContent() {
     document.getElementById('jeu-content').style.display = 'block';
+    document.getElementById("jeuID").classList.add('active');
 }
 
 function showChatContent() {
     document.getElementById('chat-content').style.display = 'block';
+    document.getElementById("chatID").classList.add('active');
 }
 
 function showAccueilContent() {
     document.querySelector('#accueil-content').style.display = 'block';
+    document.getElementById("accID").classList.add('active');
 }
 
 function showNavBar() {
@@ -44,7 +44,6 @@ function showNavBar() {
 
 
 document.querySelector('a[href="#jeu"]').addEventListener('click', function(event) {
-    event.preventDefault(); 
     hideJeuContent(); 
     hideChatContent(); 
     hideAccueilContent();
@@ -52,7 +51,6 @@ document.querySelector('a[href="#jeu"]').addEventListener('click', function(even
 });
 
 document.querySelector('a[href="#chat"]').addEventListener('click', function(event) {
-    event.preventDefault(); 
     hideChatContent(); 
     hideJeuContent();
     hideAccueilContent();
@@ -60,30 +58,56 @@ document.querySelector('a[href="#chat"]').addEventListener('click', function(eve
 });
 
 document.querySelector('a[href="#accueil"]').addEventListener('click', function(event) {
-    event.preventDefault(); 
     hideChatContent(); 
     hideJeuContent();
     hideAccueilContent();
     showAccueilContent();
 });
 
-document.querySelectorAll('.nav-link').forEach(link => {
-    if (link.getAttribute('href') !== '#jeu') { 
-        link.addEventListener('click', function(event) {
-            event.preventDefault(); 
-            hideJeuContent(); 
-        });
+function selectPage()
+{
+    if (location.hash === "#jeu")
+    {
+        hideJeuContent(); 
+        hideChatContent(); 
+        hideAccueilContent();
+        showJeuContent(); 
     }
-    else if (link.getAttribute('href') !== '#chat') { 
-        link.addEventListener('click', function(event) {
-            event.preventDefault(); 
-            hideChatContent(); 
-        });
+    else if (location.hash === "#chat")
+    {
+        hideChatContent(); 
+        hideJeuContent();
+        hideAccueilContent();
+        showChatContent(); 
     }
-    else if (link.getAttribute('href') !== '#accueil') { 
-        link.addEventListener('click', function(event) {
-            event.preventDefault(); 
-            hideAccueilContent(); 
-        });
+    else if (location.hash === "#accueil")
+    {
+        hideChatContent(); 
+        hideJeuContent();
+        hideAccueilContent();
+        showAccueilContent();
     }
-});
+}
+
+// document.querySelectorAll('.nav-link').forEach(link => {
+//     if (link.getAttribute('href') !== '#jeu') { 
+//         link.addEventListener('click', function(event) {
+//             // event.preventDefault(); 
+//             hideJeuContent(); 
+//         });
+//     }
+//     else if (link.getAttribute('href') !== '#chat') { 
+//         link.addEventListener('click', function(event) {
+//             // event.preventDefault(); 
+//             hideChatContent(); 
+//         });
+//     }
+//     else if (link.getAttribute('href') !== '#accueil') { 
+//         link.addEventListener('click', function(event) {
+//             // event.preventDefault(); 
+//             hideAccueilContent(); 
+//         });
+//     }
+// });
+
+selectPage();
