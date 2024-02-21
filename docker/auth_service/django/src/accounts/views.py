9ec2d85691c7326b5d848fb67d41778c727024bf
Islem Kaismoun <ikaismou@z2r10p2.42lyon.fr>
2024-02-21 19:06:37 +0100

    Serializer workin with hashing password and registering in database

--- docker/auth_service/django/src/accounts/views.py
+++ docker/auth_service/django/src/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from accounts.serializers import UserSerializer
 
-# Create your views here.
+class UserCreate(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
