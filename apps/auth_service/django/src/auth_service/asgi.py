c4e109c05bed05710b7928a13c5106a8b89e8cb8
titi-rex <13titirex@pm.me>
2024-02-22 15:04:08 +0100

    wip

--- apps/auth_service/django/src/auth_service/asgi.py
+++ apps/auth_service/django/src/auth_service/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for game project.
+ASGI config for auth_service project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth_service.settings')
 
 application = get_asgi_application()
