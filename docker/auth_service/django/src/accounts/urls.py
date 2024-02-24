4ba4e00eee44e2e76c87b206427ecb732134159a
Islem Kaismoun <ikaismou@z2r10p2.42lyon.fr>
2024-02-24 16:20:19 +0100

    Added ratelimit connexion attempts by 5/min

--- docker/auth_service/django/src/accounts/urls.py
+++ docker/auth_service/django/src/accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from accounts.views import UserCreate
+from accounts.views import UserCreate, CustomTokenRefreshView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('api/signin/', UserCreate.as_view(), name='signup'),
+    path('api/signup/', UserCreate.as_view(), name='signup'),
 	path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
 ]
--- docker/auth_service/django/src/accounts/views.py
+++ docker/auth_service/django/src/accounts/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from accounts.serializers import UserSerializer
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework.throttling import AnonRateThrottle
 
 class UserCreate(APIView):
     def post(self, request):
@@ -11,3 +13,6 @@ class UserCreate(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CustomTokenRefreshView(TokenRefreshView):
+    throttle_classes = (AnonRateThrottle,)
