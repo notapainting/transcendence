b715d83ab6766a2f8299026ed92da4c0100b6e8b
Islem Kaismoun <ikaismou@z2r10p2.42lyon.fr>
2024-02-24 16:20:19 +0100

    Added ratelimit connexion attempts by 5/min

--- docker/auth_service/django/src/auth_service/settings.py
+++ docker/auth_service/django/src/auth_service/settings.py
@@ -32,6 +32,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+	'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/min'
+    }
 }
 
 
