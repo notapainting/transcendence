0b0481598e33a7a79a5bf74e2c3c1bac3adec6ea
islemk69 <islemkaismoun@gmail.com>
2024-02-27 21:44:18 +0100

    added eail verification

--- docker/auth_service/django/src/accounts/views.py
+++ docker/auth_service/django/src/accounts/views.py
@@ -1,16 +1,65 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from accounts.serializers import UserSerializer
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from rest_framework.throttling import AnonRateThrottle
+from django.core.mail import send_mail
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from accounts.models import CustomUser
+from django.urls import reverse
+from django.http import HttpResponse
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.exceptions import InvalidToken, AuthenticationFailed
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        user = self.user
+
+        if not user.isVerified:
+            raise AuthenticationFailed('Email non vérifié.')
+        return data
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+def verify_email(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode('utf-8')
+        user = CustomUser.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.isVerified = True
+        user.save()
+        return HttpResponse('Lien de vérification valide', status=200)
+    else:
+        return HttpResponse('Lien de vérification invalide ou expiré', status=400)
+
 
 class UserCreate(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            user = serializer.save()
+            token = default_token_generator.make_token(user)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            verification_url = reverse('verify_email', kwargs={'uidb64': uid, 'token': token})
+            full_verification_url = request.build_absolute_uri(verification_url)
+            send_mail(
+                'Vérifiez votre adresse email',
+                f'olalaaaaa sa marche : {full_verification_url}',
+                'jill.transcendance@gmail.com',
+                [user.email],
+                fail_silently=False,
+            )
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
