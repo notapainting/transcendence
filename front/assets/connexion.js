cdd66f51b11dbf928b2355234b0e9eff6df009a1
Islem Kaismoun <ikaismou@z2r10p2.42lyon.fr>
2024-02-28 14:42:07 +0100

    maj front

---
+++ front/assets/connexion.js
@@ -0,0 +1,31 @@
+
+document.getElementById("loginForm").addEventListener("submit", function(event){
+	event.preventDefault(); 
+	
+	var login = document.getElementById("login").value;
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+	
+	var formData = {
+		"username": login,
+		"email": email,
+		"password": password
+	};
+	
+	var formDataJSON = JSON.stringify(formData);
+	
+	// console.log(formDataJSON); // test, a supprimer
+
+	fetch('https://localhost:8000/auth/account', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: formDataJSON
+        })
+		.then(response => {
+            if (!response.ok)
+                throw new Error('Error : Bad Network response :(');
+            return response.json();
+        })
+		.then(data => console.log(data))
+		.catch(error => console.error(error));
+});
