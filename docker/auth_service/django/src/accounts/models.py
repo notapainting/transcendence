0f9bc48b1ec026998201a9cfcf24d673d2dd980c
Islem Kaismoun <ikaismou@z2r10p2.42lyon.fr>
2024-02-28 14:22:32 +0100

    merge

--- docker/auth_service/django/src/accounts/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class CustomUser(AbstractUser):
-	isVerified = models.BooleanField(default = False)
-	email = models.EmailField(unique=True)
