42e578e26d6434cd3a1e89a476b42b0fdc55a7d9
Louisa4124 <louisaaa14@gmail.com>
2024-03-19 15:36:31 +0100

    wip

--- apps/game/django/src/game/urls.py
+++ apps/game/django/src/game/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from game.views import back_test_view, start_game_view, launch_ball_view
+from game.views import back_test_view, start_game_view, paddle_view
 
 
 urlpatterns = [
 	path('api-game/game/', back_test_view, name='back_test'),
-	path('api-game/launch-ball/', launch_ball_view, name='launch_ball'),
+	path('api-game/paddle/', paddle_view, name='paddle'),
 	path('start-game/', start_game_view, name='start_game'),
 ]
