7315aca193a4b0462c9bb105844247748b0a8043
Islem Kaismoun <ikaismou@z4r11p3.42lyon.fr>
2024-04-02 15:54:38 +0200

    changed api endpoint to auth

--- apps/auth-service/django/src/auth_service/urls.py
+++ apps/auth-service/django/src/auth_service/urls.py
@@ -5,11 +5,11 @@ from auth_service.views import UserCreate, CustomTokenRefreshView, verify_email,
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
-	path('api/signup/', UserCreate.as_view(), name='signup'),
-	path('api/token/',CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-	path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
-	path('api/verify-email/<uidb64>/<token>/', verify_email, name='verify_email'),
-	path('api/reset_password/', PasswordRequestReset.as_view(), name='password_request_reset'),
-	path('api/password_reset_confirm/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-	path('api/validate_token/', ValidateTokenView.as_view(), name='validate_token'),
+	path('auth/signup/', UserCreate.as_view(), name='signup'),
+	path('auth/token/',CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+	path('auth/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+	path('auth/verify-email/<uidb64>/<token>/', verify_email, name='verify_email'),
+	path('auth/reset_password/', PasswordRequestReset.as_view(), name='password_request_reset'),
+	path('auth/password_reset_confirm/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+	path('auth/validate_token/', ValidateTokenView.as_view(), name='validate_token'),
 ]
