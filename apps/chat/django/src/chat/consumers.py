0179fc47b53c2112d1d9396392a2a5e964ad295a
titirex <github.0hi50@slmail.me>
2024-04-02 16:30:00 +0200

    wip

--- apps/chat/django/src/chat/consumers.py
+++ apps/chat/django/src/chat/consumers.py
@@ -4,6 +4,11 @@ import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+# from urllib.parse import parse_qsl
+from requests.structures import CaseInsensitiveDict
+
+
+
 import logging
 # Get an instance of a logger
 logger = logging.getLogger('django')
@@ -14,19 +19,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
-        self.userName = self.scope["query_string"]
+        # dictQuery = parse_qsl(self.scope["query_string"].decode('utf-8'))
+        # self.userName = dictQuery[0][1]
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
-        await self.accept()
+        raw = dict(self.scope['headers'])[b'cookie'].decode()
+        test = CaseInsensitiveDict(i.split('=', 1) for i in raw.split('; '))
+        self.userName = test.get('username', 'Anon')
         logger.info("%s Connected!", self.userName)
+        await self.accept()
 
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
         logger.info("%s Quit...", self.userName)
 
-        
+
 
     # Receive message from WebSocket
     async def receive(self, text_data):
