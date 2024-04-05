3405002bea918a9cf0a95546ddb47123eddf11a9
islemk69 <islemkaismoun69@gmal.com>
2024-04-05 18:34:51 +0200

    added feature prophile picture, refacto code

--- apps/auth-service/django/src/auth_service/models.py
+++ apps/auth-service/django/src/auth_service/models.py
@@ -2,7 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+
+
+
 class CustomUser(AbstractUser):
-	isVerified = models.BooleanField(default = False)
-	verification_key = models.CharField(max_length=100, blank=True)
-	is_2fa_enabled = models.BooleanField(default=False)	
+    isVerified = models.BooleanField(default=False)
+    verification_key = models.CharField(max_length=100, blank=True)
+    is_2fa_enabled = models.BooleanField(default=False)
+
+class UserToken(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='tokens')
+    access_token = models.CharField(max_length=255)
+    refresh_token = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
