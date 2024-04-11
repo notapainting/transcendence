22a0b20a1926d7ce7bd20b4f7fda29a94afee7b2
islemk69 <islemkaismoun69@gmal.com>
2024-04-11 18:25:30 +0200

    added update profile info with p-picture

--- apps/auth-service/django/src/auth_service/settings.py
+++ apps/auth-service/django/src/auth_service/settings.py
@@ -46,7 +46,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
