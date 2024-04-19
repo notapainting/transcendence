9be19d301a4fff9de458a4ddff2027b083efb723
islemk69 <islemkaismoun@gmail.com>
2024-04-19 17:41:11 +0200

    finished 2fA, have to add logic of connexion when 2fa=True

--- apps/auth-service/django/src/auth_service/models.py
+++ apps/auth-service/django/src/auth_service/models.py
@@ -11,6 +11,7 @@ class CustomUser(AbstractUser):
     is_2fa_enabled = models.BooleanField(default=False)
     is_42 = models.BooleanField(default=False)
     profile_picture = models.CharField(max_length=300, null=True, blank=True)
+    secret_key = models.CharField(max_length=100, blank=True, null=True)
 
 class UserToken(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='tokens')
