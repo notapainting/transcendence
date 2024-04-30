5ecbbe10818f8c215190b4948e5d80c64d64766c
islemk69 <islemkaismoun@gmail.com>
2024-04-30 16:48:53 +0200

    ADD: From signin

--- front/js/Home.js
+++ front/js/Home.js
@@ -2,8 +2,10 @@ export const parallaxEffect = (event) => {
     const backThrees = document.querySelector('.back-threes');
     const middleThrees = document.querySelector('.middle-threes');
     const frontThrees = document.querySelector('.front-threes');
+
     let xAxis = (window.innerWidth / 2 - event.pageX) / 10;
     let yAxis = (window.innerHeight / 2 - event.pageY) / 10;
+
     backThrees.style.backgroundPosition = `calc(50% + ${xAxis * 0.2}px) calc(50% + ${yAxis * 0.2}px)`;
     middleThrees.style.backgroundPosition = `calc(50% + ${xAxis * 0.4}px) calc(50% + ${yAxis * 0.4}px)`;
     frontThrees.style.backgroundPosition = `calc(50% + ${xAxis * 0.9}px) calc(50% + ${yAxis * 0.9}px)`;
