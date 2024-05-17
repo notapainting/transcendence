d0feeab9f2bb9090167c32d2a8f8bd0e6f904d86
BilelLk <bilel.lakehal69@gmail.com>
2024-05-17 19:29:21 +0200

    ADD : elk setup + LOGGING settings.py, nginx.conf syslog modif, MAkefile.var compose.elk modif, requirement.txt with logstash and syslog

--- apps/chat/django/src/project/urls.py
+++ apps/chat/django/src/project/urls.py
@@ -2,8 +2,18 @@
 
 from django.urls import path, include
 from chat.urls import urls
+import logging
+
+def my_view(request):
+    logging.debug('Debug message')
+    logging.info('Info message')
+    logging.warning('Warning message')
+    logging.error('Error message')
+    logging.critical('Critical message')
+    return HttpResponse("Logging test")
 
 urlpatterns = [
     path('api/', view=include(urls)),
+    path('log/', my_view),
 ]
 
