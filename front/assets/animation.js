8719774db97095556a583bf8eab8a2cf20d96840
Louiza Boudjemai <lboudjem@z3r6p4.42lyon.fr>
2024-05-22 15:21:10 +0200

    wip

--- front/assets/animation.js
+++ front/assets/animation.js
@@ -1,17 +1,11 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.163.0/build/three.module.js';
 import * as load from './loader.js';
-import { composer } from './game.js';
 import * as game from './game.js';
 import * as utils from './utils.js';
+import { composer } from './game.js';
+import { customData } from './custom.js';
 import { gameData } from './game.js';
 import { loadData } from './loader.js';
-
-export var colorsBlue = [0x00f9ff, 0x00d2ff, 0x009fff, 0x0078ff, 0x0051ff, 0x0078ff, 0x009fff, 0x00d2ff];
-export var colorsGreen = [0x132A13, 0x31572C, 0x4F772D, 0x90A955, 0xECF39E, 0x90A955, 0x4F772D, 0x31572C];
-export var colorsYellow = [0xFCEC5D, 0xFCDC5D, 0xFCCC5D, 0xFCB75D, 0xFCAC5D, 0xFCB75D, 0xFCDC5D, 0xFCDC5D];
-export var colorsOrange = [0xFF9E00, 0xFF8500, 0xFF6D00, 0xFF5400, 0xFF4800, 0xFF5400, 0xFF6D00, 0xFF8500];
-export var colorPalettes = [colorsBlue, colorsGreen, colorsYellow, colorsOrange];
-export let colorBall = colorsBlue;
 
 let acceleration = 9.8;
 let bounce_distance = 40;
@@ -19,52 +13,11 @@ let time_step = 0.06;
 let time_counter = Math.sqrt(bounce_distance * 2 / acceleration);
 let initial_speed = acceleration * time_counter;
 let bounce_height_factor = 1;
-var target = new THREE.Vector3(0, 0, 0);
-
-var p1 = { x: -45, y: -25 };
-var p2 = { x: 45, y: -25 };
-var p3 = { x: 45, y: 25 };
-var p4 = { x: -45, y: 25 };
-
-export var bonusTime = 1;
-
-const startGameButton = document.getElementById('startGame');
-const localGameButton = document.getElementById('localGame');
-const exitButton = document.getElementById('exit');
 
 export const animationData = {
 	ballFall: false,
 	intro: 0
   };
-
-localGameButton.addEventListener('click', () => {
-	// startGameButton.style.display = 'none';
-	localGameButton.style.display = 'none';
-	exitButton.style.display = 'none';
-});
-
-startGameButton.addEventListener('click', () => {
-	console.log("position:");
-	console.log(game.camera.position.x);
-	console.log(game.camera.position.y);
-	console.log(game.camera.position.z);
-	console.log("rotation:");
-	console.log(game.camera.rotation.x);
-	console.log(game.camera.rotation.y);
-	console.log(game.camera.rotation.z);
-
-	var cameraDirection = new THREE.Vector3();
-	game.camera.getWorldDirection(cameraDirection);
-	console.log('getWorldDirection:', cameraDirection);
-
-	var lookAtPoint = new THREE.Vector3();
-	lookAtPoint.copy(game.camera.position).add(cameraDirection);
-	console.log('La caméra regarde vers le point :', lookAtPoint);
-});
-
-function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-  }
 
 export async function animate() {
     requestAnimationFrame(animate);
@@ -79,8 +32,8 @@ export async function animate() {
 	if (loadData.mixerBoost)
 		loadData.mixerBoost.update(0.01); 
 
-	// if (game.sceneHandler === 1)
-	// {
+	if (gameData.sceneHandler === 1)
+	{
 		if(animationData.ballFall === true)
 		{
 			game.scene.add(game.sphere);
@@ -129,13 +82,13 @@ export async function animate() {
 		{
 			var randomIndex;
 			do {
-				randomIndex = Math.floor(Math.random() * colorPalettes.length);
-			} while (colorBall === colorPalettes[randomIndex]);
+				randomIndex = Math.floor(Math.random() * customData.colorPalettes.length);
+			} while (customData.colorBall === customData.colorPalettes[randomIndex]);
 		
-			colorBall = colorPalettes[randomIndex];
+			customData.colorBall = customData.colorPalettes[randomIndex];
 			gameData.collisionPaddle = false;
 		}
-	// }
+	}
 
     composer.render();
 }
