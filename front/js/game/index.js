f9938b1589140943ad638ad6bdfa177c185437a3
islemk69 <islemkaismoun@gmail.com>
2024-07-18 21:38:26 +0200

    FIX: showgame and showgamelocal changed to async with isUserAAuthenticated

--- front/js/game/index.js
+++ front/js/game/index.js
@@ -4,34 +4,68 @@ import { clearScene } from './utils.js';
 import { clearThree } from './game.js';
 import { clearView } from "../index.js";
 import { loggedInStatus } from '../home.js';
+import { isUserAuthenticated } from '../index.js';
 import * as enu from './enums.js'
 
+let flg2 = 0;
+let flg3 = 0;
 
-export const showGame = () => {
-    clearView();
-    fullClear();
-    loggedInStatus();
-    initGameWebSocket()
-    document.querySelector("body").style.backgroundColor = "#34A0A4"
-    document.querySelector("#game").style.display = " block"
-    setTimeout(()=> {
-        document.querySelector("#game").style.opacity = "1";
-    }, 200)
-    initMenu(enu.backendPath.REMOTE);
-    
+export const showGame = async () => {
+    await isUserAuthenticated();
+    fetch('/auth/get_pers_infos/', {
+        method: 'GET',
+        credentials: 'same-origin'
+    })
+    .then(response => {
+        if (response.ok)
+            return response.json();
+        else {
+            throw new Error("Unauthorized");
+        }
+    })
+    .then(data => {
+        clearView();
+        fullClear();
+        loggedInStatus(data.profile_picture, data.username);
+        if (!flg3)
+            initGameWebSocket();
+        flg3 = 1;
+        document.querySelector("body").style.backgroundColor = "#34A0A4"
+        document.querySelector("#game").style.display = " block"
+        setTimeout(()=> {
+            document.querySelector("#game").style.opacity = "1";
+        }, 200)
+        initMenu(enu.backendPath.LOCAL);
+    })
 }
 
-export const showGameLocal = () => {
-    clearView();
-    fullClear();
-    loggedInStatus();
-    initLocalGameWebSocket();
-    document.querySelector("body").style.backgroundColor = "#34A0A4"
-    document.querySelector("#game").style.display = " block"
-    setTimeout(()=> {
-        document.querySelector("#game").style.opacity = "1";
-    }, 200)
-    initMenu(enu.backendPath.LOCAL);
+export const showGameLocal  = async () => {
+    await isUserAuthenticated();
+    fetch('/auth/get_pers_infos/', {
+        method: 'GET',
+        credentials: 'same-origin'
+    })
+    .then(response => {
+        if (response.ok)
+            return response.json();
+        else {
+            throw new Error("Unauthorized");
+        }
+    })
+    .then(data => {
+        clearView();
+        fullClear();
+        loggedInStatus(data.profile_picture, data.username);
+        if (!flg2)
+            initLocalGameWebSocket();
+        flg2 = 1;
+        document.querySelector("body").style.backgroundColor = "#34A0A4"
+        document.querySelector("#game").style.display = " block"
+        setTimeout(()=> {
+            document.querySelector("#game").style.opacity = "1";
+        }, 200)
+        initMenu(enu.backendPath.LOCAL);
+    })
 }
 
 
