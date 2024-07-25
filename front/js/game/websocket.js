efd38acc30d1912d1b0f6ecb0dddd9421c3af0d9
Louisa4124 <louisaaa14@gmail.com>
2024-07-25 23:16:21 +0200

    ADD: invitations reçus, profil pics invitation, delete button, waiting animation

--- front/js/game/websocket.js
+++ front/js/game/websocket.js
@@ -234,27 +234,40 @@ function updateInvitationList(type, user) {
     if (type === enu.EventGame.INVITE) {
         var joinType = enu.EventGame.JOIN;
         var denyType = enu.EventGame.DENY;
-        var typeGame = "match";
+        var typeGame = "invitationTypeMatch";
+        var typeClass = "bi bi-controller";
     } else {
         var joinType = enu.EventTournament.JOIN;
         var denyType = enu.EventTournament.DENY;
-        var typeGame = "tournoi";
+        var typeGame = "invitationTypeTournament";
+        var typeClass = "bi bi-people";
     }
 
     const   item = document.createElement('li');
     const   itemName = document.createElement('span');
-    const   itemStatus = document.createElement('span');
+    const   itemStatus = document.createElement('i');
+    const   itemStatusContainer = document.createElement('div');
     const   acceptButton = document.createElement('button');
     const   refuseButton = document.createElement('button');
 
     item.className = 'list-tournoi-element';
     item.id = "invited-by-" + user;
-    itemStatus.textContent = typeGame;
+    itemStatusContainer.className = "invitationTypeContainer " + typeGame;
+    itemStatus.className = typeClass;
     itemName.textContent = user;
     acceptButton.textContent = 'Accepter';
     acceptButton.className = 'accept-button';
-    refuseButton.textContent = 'Refuse';
     refuseButton.className = 'remove-button';
+
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('viewBox', '0 0 448 512');
+    svg.setAttribute('class', 'svgIcon'); 
+
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute('d', 'M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z');
+    path.setAttribute('fill', 'white'); 
+
+    svg.appendChild(path);
 
     acceptButton.addEventListener('click', () => {
         gameSocket.send(JSON.stringify({
@@ -271,10 +284,11 @@ function updateInvitationList(type, user) {
         }));
 
     });
-
-    item.appendChild(itemStatus);
+    itemStatusContainer.appendChild(itemStatus);
+    item.appendChild(itemStatusContainer);
     item.appendChild(itemName);
     item.appendChild(acceptButton);
+    refuseButton.appendChild(svg);
     item.appendChild(refuseButton);
 
     document.getElementById('game-menu-invitationList').appendChild(item);
