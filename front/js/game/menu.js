89fa735da74e6a17e32597d41c20715de1514771
Louisa4124 <louisaaa14@gmail.com>
2024-07-28 14:20:41 +0200

    push

--- front/js/game/menu.js
+++ front/js/game/menu.js
@@ -80,7 +80,7 @@ const   scene = [
     [menuM1, invitationBox], // accueil du jeu
     [menuM2a, locContainerList, locContainerSettings], // creation de partie/tournoi (host only)
     [menuM2b], // waiting room pour creation de tournoi (guest only)
-    [menuM3, bannerPhase], // phases du tournoi : montre les prochain match de la phas eet leur etat
+    [menuM3, bannerPhase], // phases du tournoi : montre les prochain match de la phase et leur etat
     [bannerMatch, ready, circle], // afk check
     [bannerScore, pause], // in game
     [bannerEnd], // ecran de fin de match 
@@ -345,8 +345,8 @@ nextMatch.addEventListener('click', () => {
 })
 
 document.getElementById('game-menu-local-input-button').addEventListener('click', () => {
-    const   user = document.getElementById('game-menu-local-input').value;
-    document.getElementById('game-menu-local-input').value = '';
+    const   user = document.getElementById('game-menu-input-player').value;
+    document.getElementById('game-menu-input-player').value = '';
     if (validate_name(user) === false) return ;
     players.push(user);
 
@@ -361,18 +361,30 @@ document.getElementById('game-menu-local-input-button').addEventListener('click'
 
 const createListLocal = (user) => {
     const   item = document.createElement('li');
-    const   itemName = document.createElement('span');
+    const   itemName = document.createElement('div');
     const   button = document.createElement('button');
-
+    
     item.className = 'list-tournoi-element';
     itemName.textContent = user;
-    button.textContent = 'Remove';
-    button.className = 'accept-button';
+    itemName.className = 'list-tournoi-user-name';
+    // button.textContent = 'Remove';
+    button.className = 'remove-button';
     button.addEventListener('click', (e) => {
         const pos = players.indexOf(user);
         players.splice(pos, 1);
         e.target.parentElement.remove();
     });
+
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('viewBox', '0 0 448 512');
+    svg.setAttribute('class', 'svgIcon'); 
+
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute('d', 'M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z');
+    path.setAttribute('fill', 'white'); 
+
+    svg.appendChild(path);
+    button.appendChild(svg);
     item.appendChild(itemName);
     item.appendChild(button);
     document.getElementById('game-menu-list').appendChild(item);
