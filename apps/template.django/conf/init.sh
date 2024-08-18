26c987ce093109d862eb30648308ac20a0057a80
reina <13titirex@pm.me>
2024-08-18 18:58:21 +0200

    FIX: daphne django start with dapnhe automatically (chat, game)

--- apps/template.django/conf/init.sh
+++ apps/template.django/conf/init.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 python /app/manage.py migrate
+
+# development server
 python /app/manage.py runserver 0.0.0.0:8000
+# for daphne
+# daphne -b 0.0.0.0 -p 8000 game_back.asgi:application
+
