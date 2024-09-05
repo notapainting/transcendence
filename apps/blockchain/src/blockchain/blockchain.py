9780b55fc405ef7323e13ed8d99df1e78f73c176
BilelLK <bilel.lakehal69@gmail.com>
2024-09-05 07:34:52 -0400

    Try to fix

--- apps/blockchain/src/blockchain/blockchain.py
+++ apps/blockchain/src/blockchain/blockchain.py
@@ -1,18 +1,17 @@
 import os
 from web3 import Web3
 from eth_account import Account
+from eth_account.signers.local import LocalAccount
 from web3.middleware import construct_sign_and_send_raw_middleware
-import asyncio
 
 infura_api_key = os.environ.get('INFURA_API_KEY')
-if infura_api_key is None:
-    raise ValueError("La variable d'environnement INFURA_API_KEY n'est pas définie")
 
-private_key = os.environ.get('ACCOUNT_PRIVATE_KEY')
-if private_key is None:
-    raise ValueError("La variable d'environnement ACCOUNT_PRIVATE_KEY n'est pas définie")
+# Se connecter à un nœud Ethereum (nœud local ou RPC)
+# web3 = Web3(Web3.HTTPProvider(f"https://sepolia.infura.io/v3/{infura_api_key}"))
 
-web3 = Web3(Web3.HTTPProvider(f"https://sepolia.infura.io/v3/{infura_api_key}"))
+web3 = Web3(Web3.HTTPProvider(f"https://sepolia.infura.io/v3/82baf32070fa426a9603f544c7ccf3cc"))
+
+private_key = '784da8965fb39e878bec458d9ceb342b6a9ee506a7a8c16d33e95ea6ac823895'
 
 account = Account.from_key(private_key)
 
@@ -139,41 +138,38 @@ contract_abi = [
 	}
 ]
 
+contract_address = '0x2947e45e3C99e4a11E0466A8Cbecda9fD956A8CC'
+
+contract = web3.eth.contract(address=contract_address, abi=contract_abi)    
+
 import traceback
 import sys
 
-contract_address = '0x2947e45e3C99e4a11E0466A8Cbecda9fD956A8CC'
-
-contract = web3.eth.contract(address=contract_address, abi=contract_abi)
-
-async def record_match_on_blockchain(tournament_id, winner, loser, winner_score, loser_score):
+def record_match_on_blockchain(tournament_id, winner, loser, winner_score, loser_score):
     try:
-
-        nonce = web3.eth.get_transaction_count(my_account, 'pending')
-
         transaction = contract.functions.recordMatch(
-            tournament_id,
-            winner,
-            loser,
-            winner_score,
-            loser_score
-        ).build_transaction({
-            'from': my_account,
-            'nonce': nonce,
-            'gas': 2000000,
-            'gasPrice': web3.to_wei('20', 'gwei')
-        })
-        
+			tournament_id,
+			winner,
+			loser,
+			winner_score,
+			loser_score
+		).build_transaction({
+			'from': my_account,
+			'nonce': web3.eth.get_transaction_count(my_account, 'pending'),
+			'gas': web3.eth.estimate_gas({
+				'from': my_account,
+				'to': contract_address,
+				'data': contract.encodeABI(fn_name="recordMatch", args=[tournament_id, winner, loser, winner_score, loser_score])
+			}),
+			'gasPrice': web3.to_wei('100', 'gwei')
+		})
+
         signed_txn = web3.eth.account.sign_transaction(transaction, private_key)
-        
-        loop = asyncio.get_running_loop()
-        tx_hash = await loop.run_in_executor(None, web3.eth.send_raw_transaction, signed_txn.raw_transaction)
-        
-        receipt = await loop.run_in_executor(None, web3.eth.wait_for_transaction_receipt, tx_hash)
-        
+        tx_hash = web3.eth.send_raw_transaction(signed_txn.raw_transaction)
+        receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
         if receipt.status == 1:
             print("Le match a été enregistré avec succès sur la blockchain !")
-            await print_etherscan_transaction_url(tx_hash)
+            print_etherscan_transaction_url(tx_hash)
         else:
             print("L'enregistrement du match a échoué.")
     except Exception as e:
@@ -187,6 +183,6 @@ async def record_match_on_blockchain(tournament_id, winner, loser, winner_score,
         for name, value in locals().items():
             print(f"{name} = {value}")
 
-async def print_etherscan_transaction_url(tx_hash):
+def print_etherscan_transaction_url(tx_hash):
     etherscan_url = 'https://sepolia.etherscan.io/tx/'
     print("Transaction URL:", etherscan_url + tx_hash.hex())
