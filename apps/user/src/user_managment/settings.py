e3e068e0ccedf560f09b239c1070ab9f40d4ba47
titirex <github.0hi50@slmail.me>
2024-09-26 16:52:58 +0200

    fix

--- apps/user/src/user_managment/settings.py
+++ apps/user/src/user_managment/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG', False)
+DEBUG = os.getenv('DJANGO_DEBUG', True)
 
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
