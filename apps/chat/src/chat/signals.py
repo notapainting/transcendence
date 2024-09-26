980a2af1c4cab7def7f29966c417f80bacec5525
titirex <github.0hi50@slmail.me>
2024-09-26 13:50:29 +0200

    fix: clear account autocreation

--- apps/chat/src/chat/signals.py
+++ apps/chat/src/chat/signals.py
@@ -10,33 +10,8 @@ logger = getLogger('base')
 
 @receiver(post_migrate)
 def create_admin_user(sender, **kwargs):
-    if not User.objects.filter(name=settings.ADMIN_USERNAME).exists():
-        user = User.objects.create(
-            name=settings.ADMIN_USERNAME,
-        )
-        logger.info("Admin user created")
     for user in enu.SpecialUser.values:
         if not User.objects.filter(name=user).exists():
             user = User.objects.create(name=user)
             logger.info(f"Special user: {user} created")
     
-    if not User.objects.filter(name='islem').exists():
-        user = User.objects.create(
-            name='islem',
-        )
-        logger.info("islem user created")
-    if not User.objects.filter(name='lael').exists():
-        user = User.objects.create(
-            name='lael',
-        )
-        logger.info("Laël user created")
-    if not User.objects.filter(name='loulou').exists():
-        user = User.objects.create(
-            name='loulou',
-        )
-        logger.info("Loulou user created")
-    if not User.objects.filter(name='bilel').exists():
-        user = User.objects.create(
-            name='bilel',
-        )
-        logger.info("Bilel user created")
--- apps/user/src/user_managment/signals.py
+++ apps/user/src/user_managment/signals.py
@@ -11,28 +11,3 @@ def create_admin_user(sender, **kwargs):
             username=settings.ADMIN_USERNAME,
             email=settings.ADMIN_EMAIL,
         )
-        print("Admin user created")
-    if not CustomUser.objects.filter(username='islem').exists():
-        user = CustomUser.objects.create(
-            username='islem',
-            email='islem@islem.com',
-        )
-        print("islem user created")
-    if not CustomUser.objects.filter(username='lael').exists():
-        user = CustomUser.objects.create(
-            username='lael',
-            email='lael@lael.com',
-        )
-        print("lael user created")
-    if not CustomUser.objects.filter(username='loulou').exists():
-        user = CustomUser.objects.create(
-            username='loulou',
-            email='loulou@loulou.com',
-        )
-        print("loulou user created")
-    if not CustomUser.objects.filter(username='bilel').exists():
-        user = CustomUser.objects.create(
-            username='bilel',
-            email='bilel@bilel.com',
-        )
-        print("bilel user created")
